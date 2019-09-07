0090b1d9cc076fe329c346f990d02448f95181ea
nankingscolors <andersonmiko@live.com>
2019-09-07 15:39:53 -0500

    Manejo de Rutas y controladores, Peticiones Restfull Agrupadas

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/', function () {
 });
 
 
+//-------------------------------INICIO - Clase Rutas con controlador Independientes--------------------------------------------
+
+/*
+
 //ESTAS SON RUTAS QUE LLAMARAN A LOS CONTROLADORES QUE ESTAN EN LA CARPETA Http/Controllers
 
 //INICIO - Conectar una ruta a un controlador
@@ -84,3 +88,81 @@ Route::delete('categories/{category}','CategoriesController@metodoEliminar')
 ;
 
 //FIN - Ruta para eliminar una categoria 
+
+*/
+
+//-------------------------------FIN - Clase Rutas con controlador Independientes--------------------------------------------
+
+//------------------------------- INICIO - Clase Rutas con controlador Agrupadas--------------------------------------------
+//INICIO - Crear un grupo de rutas para definir las rutas de manera optima
+
+Route::group(['prefix' =>'categories'], function (){
+
+//INICIO - Conectar una ruta a un controlador
+//Ruta para listar
+
+Route::get('/categories','CategoriesController@primeraFuncion' )
+//Nombrando la ruta
+->name('categories.inicio')
+;
+
+//FIN - Conectar una ruta a un controlador
+
+//INICIO -   Creacion de una Categopria , funcion y llamado desde el controlador
+//Ruta para mostrar el formulario para crear
+
+Route::get('/categories/crear','CategoriesController@crearCategoria')
+//Nombrando ruta
+->name('categories.crear')
+;
+//FIN -   Creacion de una Categopria , funcion y llamado desde el controlador
+
+//Inicio peticiones http: POST
+//INICIO - Ruta para procesar los datos que envia
+//Ruta para  procesar el formulario
+
+Route::post('/categories','CategoriesController@almacenar')
+//Nombrando ruta
+->name('categories.almacenar')
+;
+
+//FIN - Ruta para procesar los datos que envia 
+//Fin peticiones http: POST
+
+//INICIO - Inicio peticiones Http : Metodo update
+//Ruta para actualizar los datos procesados
+//Al comenzar a utilizar variables , podemos pedirle que muestre cierto tipo de categoria, algo asi como si tengo informacion en una tabla, quie r que me muestre solo la categoria Electrodomesticos o otra 
+Route::get('categories/{category}','CategoriesController@mostrarCategoria')
+//Nombrando ruta
+->name('categories.mostrarcategoria')
+;
+
+//FIN - Inicio peticiones Http : Metodo update
+
+// INICIO - Ruta para mostrar el formulario edit 
+Route::get('/categories/{category}/edit','CategoriesController@metodoEditar')
+//Nombrando ruta
+->name('categories.editarcategoria')
+;
+// FIN - Ruta para mostrar el formulario edit
+
+
+// INICIO - Ruta para procesar y editar una categoria 
+//El verbo PUT se utiliza para actualizaciones
+Route::put('/categories/{category} ','CategoriesController@metodoActualizar')
+//Nombrando ruta
+->name('categories.editarcategoria')
+;
+// FIN - Ruta para procesar el formulario edit
+
+
+//INICIO - Ruta para eliminar una categoria 
+
+Route::delete('categories/{category}','CategoriesController@metodoEliminar')
+//Nombrando la ruta
+->name('categories.eliminarcategoria')
+;
+
+});
+//FIN - Crear un grupo de rutas para definir las rutas de manera optima
+//------------------------------- FIN - Clase Rutas con controlador Agrupadas--------------------------------------------
